3937125abb62e64c934617b5e4044af82d8be54c
Piyush <vaidpiyush.30@gmail.com>
2024-08-10 22:57:57 +0530

    Working - Using mongoDB to find and save url mapping

--- index.js
+++ index.js
@@ -7,8 +7,55 @@ const { promisify } = require("util");
 // const URL = require("node:url").URL;
 const { URL } = require("url");
 const { promises } = require("stream");
+let mongoose = require("mongoose");
 
 const app = express();
+
+mongoose.connect(process.env.MONGO_DB_URI);
+
+const urlShortenerSchema = new mongoose.Schema({
+  original_url: {
+    type: String,
+    required: true,
+  },
+  short_url: {
+    type: Number,
+    required: true,
+  },
+});
+
+const URLShortener = mongoose.model("URLShortener", urlShortenerSchema);
+
+const createAndSaveURLShortener = async (p_original_url, p_short_url, done) => {
+  const newURLShortener = new URLShortener({
+    original_url: p_original_url,
+    short_url: p_short_url,
+  });
+
+  try {
+    const data = await newURLShortener.save();
+    console.log("Data saved: ", data);
+    done(null, data);
+  } catch (error) {
+    console.log("Error in saving data");
+    console.log(error);
+    console.log("------------------------------------");
+    done(error);
+  }
+};
+
+const findOneByURL = async (p_original_url, done) => {
+  try {
+    const data = await URLShortener.findOne({ original_url: p_original_url });
+    console.log("Data found: ", data);
+    done(null, data);
+  } catch (error) {
+    console.log("Error in searching data");
+    console.log(error);
+    console.log("------------------------------------");
+    done(error);
+  }
+};
 
 // Convert dns.lookup into a Promise based function
 const dnsLookup = promisify(dns.lookup);
@@ -42,34 +89,54 @@ app.get("/api/shorturl/:urlIDval", (req, res) => {
   if (found_url_shorturl.length == 1) {
     url_to_redirect_to = found_url_shorturl[0].original_url;
   }
-  console.log(url_to_redirect_to);
+  // console.log(url_to_redirect_to);
   res.redirect(url_to_redirect_to);
 });
 
 app.post("/api/shorturl", async (req, res) => {
   try {
     const { url } = req.body;
-    let urlID_to_set = 0;
     const url_obj = new URL(url);
     const hostname = url_obj.hostname;
 
     await dnsLookup(hostname);
-
-    const found_url_shorturl = url_shorturl_map.filter(
-      (current) => current.original_url === url
-    );
-
-    // Check if host already mapped
-    if (found_url_shorturl.length == 1) {
-      urlID_to_set = found_url_shorturl[0].short_url;
-    } else {
-      urlID_to_set = short_url_id_counter;
-      short_url_id_counter += 1;
-      url_shorturl_map.push({ original_url: url, short_url: urlID_to_set });
-    }
-
-    res.json({ original_url: url, short_url: urlID_to_set });
-    console.log(url_shorturl_map);
+    URLShortener.find().sort();
+    await findOneByURL(url, async (err, data) => {
+      if (err) {
+        res.json({ error: "Error in searching in mongoDB" });
+      } else {
+        if (data) {
+          console.log("Record already found in MongoDB: ", data);
+          res.json({
+            original_url: data.original_url,
+            short_url: data.short_url,
+          });
+        } else {
+          await createAndSaveURLShortener(
+            url,
+            short_url_id_counter,
+            (err, data) => {
+              if (err) {
+                res.json({ error: "Error in saving in mongoDB" });
+              } else {
+                if (data) {
+                  console.log("New Record created in MongoDB: ", data);
+                  res.json({
+                    original_url: data.original_url,
+                    short_url: data.short_url,
+                  });
+                  short_url_id_counter += 1;
+                } else
+                  res.json({
+                    error:
+                      "No error, but looks like data is not saved in MongoDB",
+                  });
+              }
+            }
+          );
+        }
+      }
+    });
   } catch (err) {
     console.error("--------ERROR CODE: ", err.code);
     if (err.code === "ENOTFOUND") {
@@ -77,7 +144,6 @@ app.post("/api/shorturl", async (req, res) => {
     } else if (err.code === "ERR_INVALID_URL") {
       return res.json({ error: "Invalid URL" });
     }
-    console.error("DNS lookup failed:", err);
     res.json({ error: "Internal Server Error" });
   }
 });
